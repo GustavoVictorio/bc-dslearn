4684ffa8cae1f5ade2679c97debb3953b5851189
GustavoVictorio <gu.victorio3@gmail.com>
2021-02-17 10:19:29 -0300

    Backend project created

---
+++ backend/src/test/java/com/gustavoVictorio/dslearnbds/DslearnbdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavoVictorio.dslearnbds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslearnbdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
