f1318bc8872282e354029629cc727f1cd1c3492a
GustavoVictorio <gu.victorio3@gmail.com>
2021-04-07 10:26:05 -0300

    Validation, Exception handlig

---
+++ backend/src/main/java/com/gustavoVictorio/dslearnbds/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.gustavoVictorio.dslearnbds.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(String msg) {
+		super(msg);
+	}
+}
